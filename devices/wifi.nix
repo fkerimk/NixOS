da90e60c16f53b8575a57e02ebeaa1a3395e42cf
fkerimk <fkerimk26@gmail.com>
2025-05-11 17:38:10 +0300

    Damn usb wifi fixed

--- devices/wifi.nix
+++ devices/wifi.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-    networking.networkmanager.enable = true;
+    hardware.enableRedistributableFirmware = true;
 
     boot.extraModulePackages = with config.boot.kernelPackages; [
         
         rtl88xxau-aircrack
     ];
 
-    hardware.enableRedistributableFirmware = true;
+    networking.interfaces.wlp45s0f3u4.useDHCP = true;
 }
